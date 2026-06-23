65399f9814bf64f161b4f5d6b6e251199a5b1701
Gladun Vladislav <bincha.1997@gmail.com>
2026-06-24 00:23:49 +0900

    feat: add pgvector migration

--- prisma/migrations/20260622142009_pgvector_to_chunks/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `embedding` on the `document_chunks` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "document_chunks" DROP COLUMN "embedding";
